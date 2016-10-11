1173570bb096d1cec7714d54637ae976875e8579
Ian Zhang <ianzhang1986@gmail.com>
2016-10-10 17:42:54 -0700

    [Feature] Reduce dataset to increase the speed

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,6 +20,9 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+# reduce training dataset size
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
 
 #########################################################
